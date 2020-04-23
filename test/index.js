f2eac30139bb6713542baa5f7cd1900e5fff1622
CodeYHJ <colgateyhj@gmail.com>
2020-04-23 17:51:42 +0800

    test

---
+++ test/index.js
@@ -0,0 +1,20 @@
+const chai = require('chai')
+
+const sinon = require('sinon')
+
+const sinonChai = require('sinon-chai')
+
+chai.use(sinonChai);
+
+const a = require('../index')
+
+const assert = chai.assert
+
+describe('test',()=>{
+    it('is string',()=>{
+        assert.isTrue(typeof a === 'string')
+    })
+    // it('is function',()=>{
+    //     assert.isTrue(typeof a === 'function')
+    // })
+})
