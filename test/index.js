23385b7129deccdc887cf9ba6f78f70aa0214848
codeyhj <colgateyhj@gmail.com>
2020-04-24 21:46:16 +0800

    first commit3

--- test/index.js
+++ test/index.js
@@ -1,20 +1,20 @@
-const chai = require('chai')
+const chai = require("chai");
 
-const sinon = require('sinon')
+const sinon = require("sinon");
 
-const sinonChai = require('sinon-chai')
+const sinonChai = require("sinon-chai");
 
 chai.use(sinonChai);
 
-const a = require('../index')
+const a = require("../index");
 
-const assert = chai.assert
+const assert = chai.assert;
 
-describe('test',()=>{
-    it('is string',()=>{
-        assert.isTrue(typeof a === 'string')
-    })
-    // it('is function',()=>{
-    //     assert.isTrue(typeof a === 'function')
-    // })
-})
+describe("test", () => {
+  it("is string", () => {
+    assert.isTrue(typeof a === "string");
+  });
+  it("is function", () => {
+    assert.isTrue(typeof a === "function");
+  });
+});
